98eea43d183d337784197d43238c918f448f9d31
Promod <manicpromod@gmail.com>
2017-06-26 12:33:17 -0600

    updated

--- html/Contact.php
+++ html/Contact.php
@@ -129,9 +129,9 @@
         </div>
     </div>
 
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
+
+
+    </div>
     <div class ="footertag">
     </div>
   </body>
